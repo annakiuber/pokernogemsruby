e4f38ba0a151db2acdebe0a3e9360a55d19f8836
anna.kiuber <anna.kiuber@gmail.com>
2018-05-08 16:29:06 -0400

    llok at tie, pair function working

--- poker_2.rb
+++ poker_2.rb
@@ -84,12 +84,16 @@ starter_hands = hands.clone
   elsif starter_hands["hand1"] < starter_hands["hand2"]
     starter_hands["hand2"] = false
   else starter_hands["hand1"] == starter_hands["hand2"]
-      tie_breaker(hands["hand1"],hands["hand2"])
+      look_at_tie(hands["hand1"],hands["hand2"])
   end
 end
+
 def tie_breaker(hand1,hand2)
   suitvalue = []
   facevalue = []
+  array_sum1 = []
+  array_sum2 = []
+  result = ""
    "this is hand1 #{hand1}"
   hand1.each do |card|
      "this is each card #{card}"
@@ -98,21 +102,26 @@ def tie_breaker(hand1,hand2)
   end
     facevalue = facechanger(facevalue)
     facevalue.sort!
-    newarray = [*facevalue[0]..facevalue[0]+4]
-    array_sum = newarray.sum
+    newarray1 = [*facevalue[0]..facevalue[0]+4]
+    array_sum1 = newarray1.sum
 
   suitvalue = []
   facevalue = []
-   "this is hand1 #{hand1}"
   hand2.each do |card|
-     "this is each card #{card}"
     facevalue << card[0]
     suitvalue << card[1]
   end
     facevalue = facechanger(facevalue)
     facevalue.sort!
-    newarray = [*facevalue[0]..facevalue[0]+4]
-    array_sum = newarray.sum
+    newarray2 = [*facevalue[0]..facevalue[0]+4]
+    array_sum2 = newarray2.sum
+    if array_sum1 > array_sum2
+      result = "hand1"
+    else
+      result = "hand2"
+    end
+    result
+
 end
 def royal_flush(hand)
   suitvalue = []
@@ -132,18 +141,17 @@ and if facevalue includes the faces
 end
 
 def straight_flush(hand)
+  "this is straight flush"
   suitvalue = []
   facevalue = []
   hand.each do |card|
+    "this is each card #{card}"
     facevalue << card[0]
     suitvalue << card[1]
   end
   facevalue = facechanger(facevalue)
   facevalue.sort!
   newarray = [*facevalue[0]..facevalue[0]+4]
-  thearray = newarray.sum
-  result =
-  p "this is my new array of values #{thearray}"
   if suitvalue.uniq.length == 1
     if facevalue == newarray
       true
@@ -254,34 +262,56 @@ end
  end
 
 
-# def deal_with_tie(hand1,hand2)
-#   counter = 1
-#   match_card = 0
-#   hand.each do |num|
-#       hand_mock = hand1.delete_at(counter)
-#       if hand_mock.include?(num)
-#           match_card = num
-#         end
-#         counter += 1
-#       end
-#       match_card
-#     end
+ def look_at_tie(hand1,hand2)
+   # p "hand1 is #{hand1} and hand2 is #{hand2}"
+ 	array_of_faces = []
+ 	arr_of_faces2 = []
+ 	# single_element = []
+ 	result = ""
+ 	# counter = 0
+ 	match_card1 = 0
+ 	match_card2 = 0
+ 	hand1.each do |card|
+    # p hand1
+ 		hand1_separator = card.split(//)
+ 		faces_to_integers = hand1_separator[0].to_i
+ 		# counter += 1
+ 		array_of_faces << faces_to_integers
+ 	end
+ 	sorted_hand1 = array_of_faces.sort
+  hand1_dupes = sorted_hand1.join().match('(.)\1')[0]
+ 	hand1_duplicate_strings = hand1_dupes.split(//)
+  hand1_singledigit = hand1_duplicate_strings[0].to_i
 
-#     counter = 1
-#     match_card = 0
-#     hand2.each do |num|
-#       hand_mock2 = hand2.delete_at(counter)
-#       if mock_hand2.include?(num)
-#         match_card = num
-#       end
-#       counter += 1
-#     end
-#     match_card
-#   end
-#
+ 	p hand1_duplicate_strings
+  p sorted_hand1
+
+ 	hand2.each do |card|
+    # p hand2
+     	hand2_separator = card.split(//)
+     	faces_to_int2 = hand2_separator[0].to_i
+ 		# counter += 1
+ 		arr_of_faces2 << faces_to_int2
+ 	end
+ 	sorted_hand2 = arr_of_faces2.sort
+ 	hand2_dupes = sorted_hand2.join().match('(.)\1')[0]
+ 	hand2_duplicate_strings = hand2_dupes.split(//)
+  hand2_singledigit = hand2_duplicate_strings[0].to_i
+
+  p hand2_singledigit
+ 	p hand2_duplicate_strings
+  p sorted_hand2
+  
+  if hand1_singledigit > hand2_singledigit
+ 		result = "hand1 wins"
+ 	else
+ 		result = "hand2 wins"
+ 	end
+ end
+
 # def kicker_card(hand,match_card)
 #     new_hand = hand.delete(match_card)
-#     hugh_num = hand.last
+#     highh_num = hand.last
 # end
 #
 # def eval_tie(hand1,hand2)
